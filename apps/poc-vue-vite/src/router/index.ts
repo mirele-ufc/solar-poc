cb2b0f984333a68d626ee5d6837878d696653a6a
Ingrid Maria <ingrid@virtual.ufc.br>
2026-04-07 16:01:38 -0300

    fix: vue router

--- apps/poc-vue-vite/src/router/index.ts
+++ apps/poc-vue-vite/src/router/index.ts
@@ -1,11 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '../store/useAuthStore'
-
 import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      redirect: '/login' 
+    },
     {
       path: '/login',
       name: 'login',
